4664d5090d6d4b5d80c50c82ae6d76c0069eee7f
Sam Makhlouf <makhloufsam26@gmail.com>
2019-04-29 13:54:34 -0400

    'secondh'

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 
 import "./App.css";
 import GetQuote from "./GetQuote";
+import GetQuotes from "./GetQuotes";
 
 class App extends Component {
    render() {
       return (
          <div className="App">
-            <GetQuote />
+            <GetQuotes />
          </div>
       );
    }
 }
-
+//<GetQuote />
 export default App;
---
+++ src/GetQuotes.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./Quote.css";
+
+export default class GetQuote extends Component {
+   constructor() {
+      super();
+      this.state = {
+         quote: "",
+         author: ""
+      };
+   }
+   change = () => {
+      const colors = ["#ff9665", "#1a9f9b", "#b61381", "#64bbaa", "#3d484d", "#1acec5", "#74f016", "#a96363", "#ff9797", "#33f"];
+      let random = Math.floor(Math.random() * colors.length);
+      document.body.style.backgroundColor = colors[random];
+   };
+   async componentDidMount() {
+      const res = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+
+      const json = await res.json();
+
+      // console.log(json[0].quote);
+      let quote = json[0].quote;
+      let auth = json[0].character;
+      this.setState({
+         quote: quote,
+         author: auth
+      });
+
+      this.change();
+   }
+
+   render() {
+      return (
+         <div id="quote-box" className="box">
+            <p id="text">
+               <i className="fas fa-quote-left" />
+               {this.state.quote}
+            </p>
+            <h6 id="author">- {this.state.author}</h6>
+            <div className="btn">
+               <div id="new-quote" className="social">
+                  <a id="tweet-quote" href="twitter.com/intent/tweet">
+                     <i className="fab fa-twitter-square" />
+                  </a>
+                  <a href="#">
+                     <i className="fab fa-tumblr-square" />
+                  </a>
+               </div>
+               <button onClick={this.componentDidMount.bind(this)}>New One</button>
+            </div>
+         </div>
+      );
+   }
+}
